69c4fe6c5930135f87fc1dfa23a5495a34af56d5
Giedre Liekiene <giedre.valanciute@gmail.com>
2022-08-24 22:15:15 +0300

    Added new Q&A

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+//1. Write a function that takes two numbers (a and b) as Sum a and Return the result
+
+const function1 = (a, b) => a + b;
+
+//2.Write a function that takes a string as an argument. Extract the last 3 characters from the string. Return the result
 const member = "my name is Giedre";
 
 const last3 = member.slice(-3);
